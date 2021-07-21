d09bdff661a754c8cf680399c01d387e380a39ee
carthick <challakarthik@gmail.com>
2021-07-21 14:38:25 -0400

    deploy react experiments

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        My React EXPERIMENTS
+      </h1>
+     <h2>
+       Author: Challa Karthik
+     </h2>
+     
+     
+    </div>
+  );
+}
+
+export default App;
